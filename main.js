9b64f17be408af4706c6370d85c6966d70974a14
Dorothyhung <dorothyh0602@gmail.com>
2023-01-28 18:21:46 -0800

    add() and mostBurned() functions added into js, added css, cleaned up html

--- main.js
+++ main.js
@@ -1,30 +1,64 @@
 array = [];
 
 //constructor
-var Workout = function (exercise, minutes, calperminute) {
+var Workout = function (exercise, minutes, calperminute, calsBurned) {
     this.exercise = exercise;
     this.minutes = minutes;
     this.calperminute = calperminute;
-}
-
-//create object instances - need to factor in radio buttons
-
-function situps() {
-    var situps = new Workout("situps", Document.getElementById("minutes"), 10);
-    totalCals = situps.calcCalories;
-}
-
-function pushups() {
-    var pushups = new Workout("pushups", Document.getElementById("minutes"), 15);
-    totalCals = pushups.calcCalories;
-}
-
-function jumprope() {
-    var jumprope = new Workout("jumprope", Document.getElementById("minutes"), 18);
+    this.calsBurned = calsBurned;
 }
 
 
-//Calculates total calories for a workout
-Workout.calcCalories = function() {
-    return (this.minutes * this.calperminute);
+//add Workout objects to array from document
+function add() {
+    //find which radio button is checked
+    var radioButtonGroup = document.getElementsByName("exercise");
+    var checkedRadio;
+    for (i = 0; i < radioButtonGroup.length; i++) {
+        if (radioButtonGroup[i].checked)
+        checkedRadio = radioButtonGroup[i].value;
+    }
+    //get minutes
+    var minutes = parseInt(document.getElementById("minutes").value);
+    calsPerMin= getCalsPerMin(checkedRadio);
+    var calsBurned = minutes * calsPerMin;
+    var checkedExercise = new Workout(checkedRadio, minutes, calsPerMin, calsBurned);
+    array.push(checkedExercise); //adds workout to array
+    clear();
+    console.log(array);
+}
+
+
+//returns cals per minute for checked exercise
+function getCalsPerMin(checkedRadio) {
+    if (checkedRadio == "situps") {
+        calsPerMin = 10;
+    }
+    else if (checkedRadio == "pushups") {
+        calsPerMin = 15;
+    }
+    else {
+        calsPerMin = 18;
+    }
+    return calsPerMin;
+}
+
+
+//clears minutes textbox
+function clear() {
+    document.getElementById("minutes").value = "";
+}
+
+
+//adds the exercise and calorie count of highest calories burned
+function mostBurned() {
+    var highestExercise = "situps";
+    var highestCals = array[0].calsBurned;
+    for (i = 0; i < array.length; i++) {
+        if (array[i].calsBurned > highestCals) {
+            highestCals = array[i].calsBurned;
+            highestExercise = array[i].exercise;
+        }
+    }
+    document.getElementById("highestCals").value = highestExercise + " " +highestCals;
 }
